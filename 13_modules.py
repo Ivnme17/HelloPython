b10cb5f70dbb3ee6a91a08c4bce00b4348537e15
Ivnm17 <Ivnm17@gmail.com>
2026-02-11 10:47:21 +0100

    Finalizacion de modulos

---
+++ 13_modules.py
@@ -0,0 +1,17 @@
+# Modulos o Librerias
+
+#import my_module
+from my_module import sum, printValue
+import math 
+'''
+from 10_functions import suma_dos_numeros 
+nomenclatura de importacion no valida debido 
+a que el nombre del archivo contiene numeros
+''' 
+#my_module.sum(1, 2, 3)
+#my_module.printValue("Hola")
+sum(1, 2, 3)
+printValue("Hola")
+
+print(math.pi)
+print(math.pow(2, 8))
